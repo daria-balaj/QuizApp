ad34ed5cd5f36276711eb042674f1f1f49a93fa1
daria <dariabalaj7@gmail.com>
2025-02-27 21:23:25 +0200

    created angular project

---
+++ client/src/app/components/nav/nav.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-nav',
+  imports: [
+    MatIconModule
+  ],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.scss'
+})
+export class NavComponent {
+
+}
