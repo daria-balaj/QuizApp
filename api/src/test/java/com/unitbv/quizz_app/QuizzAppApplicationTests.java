f3ed92e0976961fd0e03a4cfc7457d37259ac81a
Beatrice Moraru <morarubeatrice77@yahoo.com>
2025-03-05 21:48:42 +0200

    Create Spring Boot project

---
+++ api/src/test/java/com/unitbv/quizz_app/QuizzAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unitbv.quizz_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
